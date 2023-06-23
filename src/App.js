9acf4e086e5c512e072acf4e9246cbab57325873
HEMALATHA01492 <hema01492@gmail.com>
2023-06-23 18:40:52 +0530

    day9 finished

--- src/App.js
+++ src/App.js
@@ -1,108 +1,21 @@
-// //-----DAY 8-------
-// //1.Props Drilling
+//-----------Day 9-------------
+//useRef-hook;
+//to create  a mutable reference to an element or a value that persists across render cycles
 
-
-// import React from 'react';
-// function GrandChildComponent(props){
-//     return(
-//         <div>
-//             <h3>Grand Child Component</h3>
-//             <p>{ props.message}</p>
-//         </div>
-//     )
-// }
-
-
-// function ChildComponent(props){
-//     // const message='Hello,GrandChild';
-   
-//     return(
-//         <div>
-//             <h2>Child Component</h2>
-//             <p>{ props.message}</p>
-//             <GrandChildComponent message={props.message}  />
-//             {/* <GrandChildComponent message={message}  /> */}
-
-//         </div>
-//     )
-// }
-
-// function App() {
-
-//     //Parent Component Data
-//     const message='Hello, Child';
-//   return (
-//     <div>
-//         <h1>Parent Component</h1>
-//         <ChildComponent message={message} />
-//     </div>
-//   )
-// }
-
-// export default App;
-//---------------------------------------------------//
-// 2.Create contact
-// import React,{createContext, useContext, useState, } from 'react';
-// const MessageContext=createContext();
-// function ChildComponent(){
-//     const message=useContext(MessageContext);
-//     return(
-//         <div>
-//             <h2>ChildComponent</h2>
-//             <p>{message}</p>
-//         </div>
-//     )
-    
-
-// }
-// function App() {
-//     const [message,setMessage]=useState('Hello from App');
-//     return (
-//       <div>
-//         <h1>Parent Component</h1>
-//         <p>{message}</p>
-//         <MessageContext.Provider value={message}>
-//             <ChildComponent />
-//         </MessageContext.Provider>
-//       </div>
-//     )
-//   }
-    
-
-// export default App;
-
-//Class TASK Create Profile name change
-
-import React, { createContext, useContext, useState } from 'react';
-
-const ProfileContext=createContext();
-
-function Profile(){
-    const {profileName,setProfileName}=useContext(ProfileContext);
-   let onChangeProfileNameHandler=(event)=>{
-    setProfileName(event.target.value)
-   // console.log(event.target.value)
-   }
-   
-    return(
-        <div>
-            <h2>Profile Name:{profileName}</h2>
-            <input
-            type='text'
-            value={profileName} 
-            onChange={onChangeProfileNameHandler}/>
-        </div>
-    )
-}
+import React, { useRef } from 'react';
 
 function App() {
-    const [profileName, setProfileName]=useState('');
+
+    const inputRef= useRef(null);
+    let handButtonClick=() =>{
+        //handle the input text
+       // console.log(inputRef)
+        inputRef.current.focus();
+    }
   return (
     <div>
-        <ProfileContext.Provider value={{profileName,setProfileName}}>
-        <Profile />
-        </ProfileContext.Provider>
-        
+ <input types='text' />
+ <button onClick={handButtonClick}>Focus Input</button>
     </div>
   )
 }
