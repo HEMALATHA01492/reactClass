8ce1cfdc1f6b71293231a3e70e9dbef9399576b0
HEMALATHA01492 <hema01492@gmail.com>
2023-06-26 14:06:58 +0530

    reactDay 10 finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-//-----------Day 9-------------
-//useRef-hook;
-//to create  a mutable reference to an element or a value that persists across render cycles
+//day 10 A simple Note Taking application
+// import React from 'react';
+// function App(props) {
+//   const {notes} = props;
+//   console.log(notes);
 
-import React, { useRef } from 'react';
+//   return (
+//     <div> 
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note => <li key={note.id}>{note.content}</li>)}
+//       </ul>
+      
+//     </div>
 
-function App() {
+//   )
+// }
 
-    const inputRef= useRef(null);
-    let handButtonClick=() =>{
-        //handle the input text
-       // console.log(inputRef)
-        inputRef.current.focus();
-    }
+// export default App;
+
+import React, { useState } from "react";
+function Note({note}) {
+  return(
+  <li>{note.content}</li>
+  )
+
+}
+
+function App( props ) {
+ //console.log(notes);
+ //states for notes
+ const [notes ,setNotes] =useState(props.notes);
+ const [newNote, setNewNote]=useState('...new note');
+
+ //define addNote Method
+ let addNote=(event)=>{
+  event.preventDefault();
+  // console.log("button clicked");
+
+  let noteObject={
+    id:notes.length + 1,
+    content: "newNote",
+    important: Math.random() <0.5,
+  }
+
+  setNewNote(notes.concat(noteObject));
+
+  setNewNote('...new note')
+ }
+ let handleNoteChange=(event) =>{
+  setNewNote(event.target.value);
+  console.log(newNote);
+ }
+
   return (
-    <div>
- <input types='text' />
- <button onClick={handButtonClick}>Focus Input</button>
+    <div> 
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+           <Note key={note.id} note={note}/>
+           )}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">Add Note</button>
+      </form>
+      
     </div>
+
   )
 }
 
 export default App;
+    
--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@
 import React from "react";
 import ReactDOM from  'react-dom/client';
 import App from './App';
+const notes =[
+     { id:1,
+      content: "working with lists",
+      important: true
+    },
+    { id:2,
+      content: "Usage of keys",
+      important: false
+    },
+    
+    { id:3,
+      content: "Conditional Rendering",
+      important: true
+    },
+    { id:4,
+      content: "Handling Events",
+      important: true
+    },
+    { id:5,
+      content: "Props Vs State",
+      important: false
+    }
+    ]
+    ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
 
 
 
@@ -12,5 +36,4 @@ import App from './App';
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(< App />);
 
